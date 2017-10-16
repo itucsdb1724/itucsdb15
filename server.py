0641b326b3222bcc424f66c84369daf2c14605dc
Lutfi Demirci <lutfidemirci@gmail.com>
2017-10-17 01:46:21 +0300

    Added get_elephantsql_dsn function to server.py

--- server.py
+++ server.py
@@ -1,7 +1,18 @@
 import os
+import json
+import re
 
 from app import app
 
+def get_elephantsql_dsn(vcap_services):
+    """Returns the data source name for ElephantSQL."""
+    parsed = json.loads(vcap_services)
+    uri = parsed["elephantsql"][0]["credentials"]["uri"]
+    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
+    user, password, host, _, port, dbname = match.groups()
+    dsn = """user='{}' password='{}' host='{}' port={}
+             dbname='{}'""".format(user, password, host, port, dbname)
+    return dsn
 
 VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
 if VCAP_APP_PORT is not None:
