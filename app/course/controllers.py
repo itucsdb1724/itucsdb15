d1a7a6788bb5de157939c3f51bd9b28c8da02abb
Mert Kartaltepe <mertkartaltepe@gmail.com>
2017-12-09 03:21:49 +0200

    Add course search page

--- app/course/controllers.py
+++ app/course/controllers.py
@@ -4,9 +4,20 @@ from flask import Blueprint, request, render_template, flash, g, session, redire
 
 from flask_login import login_required, logout_user
 
+from app.course.models import CourseRepository
+
 
 course = Blueprint('course', __name__, url_prefix='/course')
 
 @course.route('/<int:id>', methods=['GET'])
 def index(id):
-    return render_template("course/index.html")
+	course = CourseRepository.find_by_id(id)
+	return render_template("course/index.html", course=course)
+
+
+# /search yapılacak
+@course.route('/search', methods=['GET'])
+def search():
+	query = request.args.get('query')
+	courses = CourseRepository.search(query)
+	return render_template("course/search.html", query=query, courses=courses)
