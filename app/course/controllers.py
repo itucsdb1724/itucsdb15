31ea4c5f872f839be159e10bb0298f1611452f76
Mert Kartaltepe <mertkartaltepe@gmail.com>
2017-12-11 17:05:11 +0200

    find_department_codes method added

--- app/course/controllers.py
+++ app/course/controllers.py
@@ -5,6 +5,7 @@ from flask import Blueprint, request, render_template, flash, g, session, redire
 from flask_login import login_required, logout_user
 
 from app.course.models import CourseRepository
+from app.section.models import SectionRepository
 
 
 course = Blueprint('course', __name__, url_prefix='/course')
@@ -12,7 +13,10 @@ course = Blueprint('course', __name__, url_prefix='/course')
 @course.route('/<int:id>', methods=['GET'])
 def index(id):
 	course = CourseRepository.find_by_id(id)
-	return render_template("course/index.html", course=course)
+	if not course:
+		abort(404)
+	sections = SectionRepository.find_sections_of_course(id)
+	return render_template("course/index.html", course=course, sections=sections)
 
 
 # /search yapılacak
@@ -21,3 +25,13 @@ def search():
 	query = request.args.get('query')
 	courses = CourseRepository.search(query)
 	return render_template("course/search.html", query=query, courses=courses)
+
+
+@course.route('/find', methods=['GET'])
+def find():
+	department_code = request.args.get('department_code')
+	course_code = request.args.get('course_code')
+	course = CourseRepository.find_by_department_and_course_code(department_code, course_code)
+	if course is not None:
+		return redirect(url_for('course.index', id=course.id))
+	return render_template("course/no-course.html", department_code=department_code, course_code=course_code)
