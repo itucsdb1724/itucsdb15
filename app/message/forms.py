1b8c2bbfeefb9e8f354ff6b5cd120adf152bee62
Lutfi Demirci <lutfidemirci@gmail.com>
2017-12-12 00:23:38 +0300

    Course view template fixes

--- app/message/forms.py
+++ app/message/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, StringField, SubmitField, validators
+from wtforms import BooleanField, StringField, SubmitField, TextAreaField, validators
 
 
 class NewThreadForSectionForm(FlaskForm):
     title = StringField('Title', [validators.Required(message='You must give a title'),
                                   validators.Length(min=4, max=50)])
-    message = StringField('Message', [validators.Required(message='Won\'t you write anything my dear.'),
+    message = TextAreaField('Message', [validators.Required(message='Won\'t you write anything my dear.'),
                                   validators.Length(min=5, max=1000)])
     section_only = BooleanField('Section only?')
     submit = SubmitField('Submit')
@@ -14,11 +14,11 @@ class NewThreadForSectionForm(FlaskForm):
 class NewThreadForCourseForm(FlaskForm):
     title = StringField('Title', [validators.Required(message='You must give a title'),
                                   validators.Length(min=4, max=5)])
-    message = StringField('Message', [validators.Required(message='Won\'t you write anything my dear.'),
+    message = TextAreaField('Message', [validators.Required(message='Won\'t you write anything my dear.'),
                                   validators.Length(min=5, max=1000)])
     submit = SubmitField('Submit')
 
 class NewMessageForm(FlaskForm):
-    message = StringField('Message', [validators.Required(message='Won\'t you write anything my dear.'),
+    message = TextAreaField('Message', [validators.Required(message='Won\'t you write anything my dear.'),
                                   validators.Length(min=5, max=1000)])
     submit = SubmitField('Submit')
